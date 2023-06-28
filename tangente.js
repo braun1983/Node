cc2081eb265f351ea56b3855a99b4e68bce9d3ab
Raphael Brun <73260233+braun1983@users.noreply.github.com>
2023-06-28 12:01:36 -0300

    mudança de nome

--- tangente.js
+++ tangente.js
@@ -1,13 +1,24 @@
-var cateto_adjacente = document.querySelector("#cateto__adjacente")
-var angulo = document.querySelector("#angulo")
 
-var btn = doc
+const btn = document.querySelector("#btn_calcula");
+btn.addEventListener("click", calcula_tangente);
 
-function calcula_tangente(graus){
-    var radianos= graus *(Math.PI/180)
-    var tangente =Math.tan(radianos)
-    var cateto_oposto = tangente*cateto_adjacente
-    console.log(cateto_oposto)
+function calcula_tangente(){
+    
+    const cateto_adjacente = document.querySelector("#cateto__adjacente")
+    const angulo = document.querySelector("#angulo")
+    
+    const graus = angulo.value
+    const cat_adjacente = cateto_adjacente.value
+
+    const radianos= graus *(Math.PI/180)
+    const tangente =Math.tan(radianos)
+    const cateto_oposto = tangente*cat_adjacente
+        escreveResultado(cateto_oposto)
 }
 
-calcula_tangente(angulo)
+function escreveResultado(resultado){
+    const resultado_saida = document.querySelector("#resultado")
+
+    resultado_saida.value = resultado;
+
+}
